a685ec61d3918456731ebf1d208d0eb47b4b5f56
Jezeniel Zapanta <jezeniel.zapanta@gmail.com>
2019-08-29 02:02:05 +0800

    Ignore .sass-cache

--- pelicanconf.py
+++ pelicanconf.py
@@ -57,7 +57,7 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 
-IGNORE_FILES = ['.#*', '*.sass', '*.scss', '_sass']
+IGNORE_FILES = ['.#*', '*.sass', '*.scss', '_sass', '.sass-cache']
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
